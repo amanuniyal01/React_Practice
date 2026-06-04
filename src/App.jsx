21e273fd1fb7526387ea137af7f4d91ba7d1f9e7
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-04 21:52:15 +0530

    Learned about states and Event handlers through a Display your choice input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ProductDetails from './components/PropsAndComponents/ProductDetails'
+import DisplayAsYouWant from './components/StatesAndEventHandlers/DisplayAsYouWant'
 
 function App() {
   return (
     <>
-      <p>Hello, Welcome to my website.</p>
-      <ProductDetails heading="Cotton Socks" para="Price : $10.9" discountPrice="$5.45" />
+      <p className='text-center font-bold'>Hello, Welcome to my website.</p>
+      {/* <ProductDetails heading="Cotton Socks" para="Price : $10.9" discountPrice="$5.45" />
       <ProductDetails heading="Plain T-shirt" para="Price : $20.9" discountPrice="" />
-      <ProductDetails heading="Tennis Ball" para="Price : $15.5" discountPrice="" />
+      <ProductDetails heading="Tennis Ball" para="Price : $15.5" discountPrice="" /> */}
+      <DisplayAsYouWant />
+
     </>
   )
 }
---
+++ src/components/StatesAndEventHandlers/DisplayAsYouWant.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+function DisplayAsYouWant() {
+    const [input, setInput] = useState("")
+    const [isTextShow, setIsTextShow] = useState(false)
+    const handleReset = () => {
+        setInput("")
+        setIsTextShow(false)
+    }
+    const handleExample = () => setInput("Hello States")
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            setIsTextShow(true)
+        }
+        if (e.key === "Escape") {
+            setInput("")
+        }
+
+    }
+
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white shadow-lg rounded-2xl p-6 w-full max-w-md text-center">
+
+                <label className="block text-lg font-semibold mb-2 text-gray-700">
+                    Enter Text You Want to Display
+                </label>
+
+                <input
+                    className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Enter the text"
+                />
+
+                <div className="flex justify-center gap-4 mt-4">
+                    <button
+                        onClick={() => setIsTextShow(true)}
+                        className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg transition"
+                    >
+                        show
+                    </button>
+                    <button
+                        onClick={handleReset}
+                        className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
+                    >
+                        Reset
+                    </button>
+
+                    <button
+                        onClick={handleExample}
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg transition"
+                    >
+                        Example
+                    </button>
+                </div>
+
+                {isTextShow && <p className="mt-6 text-xl font-medium text-gray-800 ">
+                    {input || "Your text will appear here..."}
+                </p>}
+
+            </div>
+        </div>
+    )
+}
+
+export default DisplayAsYouWant
