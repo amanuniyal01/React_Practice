a2645d0f4ce7ccb1d152f7b71f63ecc666941f28
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-11 10:53:40 +0530

    Added clear all and rewmove chips btn

--- src/components/ChipsInput/ChipsInput.jsx
+++ src/components/ChipsInput/ChipsInput.jsx
@@ -4,6 +4,7 @@ function ChipsInput() {
     const [input, setInput] = useState("")
     const [chips, setChips] = useState([])
     const handleChips = () => {
+        if (input === "") return
         setChips(prev => [...prev, input])
         setInput("")
     }
@@ -25,12 +26,20 @@ function ChipsInput() {
                     onChange={(e) => setInput(e.target.value)}
                     className=' text-center mt-10 outline-blue-300 bg-gray-200 rounded-lg w-96 p-2' type="text"
                     placeholder="Type a chip and Press Tag" />
+                {chips.length > 0 && <span onClick={() => {
+                    setChips([])
+                }} className='bg-red-400 cursor-pointer p-2 rounded-lg'>Clear all</span>}
             </div>
-            <div className='flex mt-10 text-white flex-wrap gap-10 items-center justify-center '>
+            {chips.length > 0 && <div>
+                <input className=' text-center mt-10
+            outline-blue-300 bg-gray-200 rounded-lg w-96 p-2'
+                    type="text" placeholder='Search for Chip' />
+            </div>}
+            <div className='flex mt-10 w-dvw text-white flex-wrap gap-10 items-center justify-center '>
                 {chips.map((chip, index) => (
                     <span className='cursor-pointer bg-blue-400 p-3 rounded-lg' key={index}>
                         {chip}
-                        <span onClick={() => handleRemove(index)} className="text-red-500 font-bold ml-4 p-2 rounded-lg">X</span>
+                        <span onClick={() => handleRemove(index)} className="text-red-500 font-bold ml-4 p-1 rounded-lg">X</span>
                     </span>
                 ))}
             </div>
